b41a9e6c6ee0172bf227b1fe126aad95efd0923b
Grellinn <grellinn@gmail.com>
2014-05-11 19:15:19 +0000

    Búið að gera Install-Package EntityFramework

--- Stjornutextar/Controllers/HomeController.cs
+++ Stjornutextar/Controllers/HomeController.cs
@@ -8,6 +8,16 @@ namespace Stjornutextar.Controllers
 {
 	public class HomeController : Controller
 	{
+		// TODO:
+		/* 0 Install-Package EntityFramework
+		 * 1 Model class
+		 * 2 Context class
+		 * 3 Initializer class
+		 * 4 reference initializer in web.config.
+		 * 5 add connection string in web.config.
+		 * 6 create scaffolding from model and data context.
+		 */
+		
 		public ActionResult Index()
 		{
 			return View();
