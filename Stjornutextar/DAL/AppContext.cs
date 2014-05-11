fcdaf38da7021fc25efefcf2e6510905e3667279
Grellinn <grellinn@gmail.com>
2014-05-11 19:41:06 +0000

    AppContext og AppInitializer bætt við og stillt

---
+++ Stjornutextar/DAL/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Stjornutextar.DAL
+{
+	public class AppContext : DbContext
+	{
+		// Látum default constructorinn vera tengingu við AppContext reference-ið í web.config
+		public AppContext()
+			: base("AppContext")
+		{
+
+		}
+
+		// Búum töflur fyrir gagnagrunninn sem á að mappa við klasana.
+		//public DbSet<NewsItem> News { get; set; }
+
+		// Kemur í veg fyrir að EntityFramework-ið breyti nafni töflunnar í fleirtölu þegar hún býr hana til.
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
---
+++ Stjornutextar/DAL/AppInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stjornutextar.DAL
+{
+	public class AppInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<AppContext>
+	{
+	}
+}
