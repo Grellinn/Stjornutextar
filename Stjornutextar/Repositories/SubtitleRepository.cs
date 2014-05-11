5e90140849ca588186c76aa90bb867846eac2d0f
Grellinn <grellinn@gmail.com>
2014-05-11 20:32:11 +0000

    SubtitleRepository búinn til og tengdur við AppContext

---
+++ Stjornutextar/Repositories/SubtitleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Stjornutextar.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stjornutextar.Repositories
+{
+	public class SubtitleRepository
+	{
+		private static SubtitleRepository _instance;
+
+		public static SubtitleRepository Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new SubtitleRepository();
+				return _instance;
+			}
+		}
+
+		// Búum til tilvik af gagnagrunninum okkar til að vinna með.
+		AppContext db = new AppContext();
+	}
+}
