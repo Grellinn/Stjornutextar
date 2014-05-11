a3b34ecc861973136a24c953164d0750fc6c6692
Grellinn <grellinn@gmail.com>
2014-05-11 19:05:47 +0000

    Added first version of the project

---
+++ Stjornutextar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stjornutextar.MvcApplication" Language="C#" %>
---
+++ Stjornutextar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stjornutextar.Startup))]
+namespace Stjornutextar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
