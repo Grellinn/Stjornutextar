73c0cd177cdd8c4fff7ea2042188f17952dcab2f
Grellinn <grellinn@gmail.com>
2014-05-15 16:33:47 +0000

    SubtitlePartRepository búinn með Singleton

---
+++ Stjornutextar/Repositories/SubtitlePartRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stjornutextar.Repositories
+{
+	public class SubtitlePartRepository
+	{
+		private static SubtitlePartRepository _instance;
+
+		public static SubtitlePartRepository Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new SubtitlePartRepository();
+				return _instance;
+			}
+		}
+
+
+	}
+}
